963b9d275d8a3451f08435fe0f9d23cb6b6d3e2d
lau-cloud <44518262+lau-cloud@users.noreply.github.com>
2021-08-01 10:01:04 +0200

    Add files via upload

---
+++ OLYMPICS/script.R
@@ -0,0 +1,71 @@
+# Load libraries ----------------------------------------------------------
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(devtools)
+#install.packages("remotes")
+#remotes::install_github("hrbrmstr/streamgraph", force = TRUE)
+library(streamgraph)
+library(cowplot)
+library(htmlwidgets)
+
+
+olympics <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-27/olympics.csv')
+
+medals <- c("Gold", "Silver", "Bronze")
+
+oly <- olympics %>%
+  filter(medal %in% medals) %>% 
+  filter(season == "Summer") %>%
+  filter(sport == "Swimming") %>% 
+  count(team, year) %>% 
+  rename(total = "n")
+
+#remove the ones with total less than 10
+tops <- olympics %>% 
+  filter(medal %in% medals) %>% 
+  filter(season == "Summer") %>%
+  filter(sport == "Swimming") %>% 
+  count(team) %>% 
+  rename(total_total = "n") %>% 
+  arrange(total_total)
+
+#merge to filter
+oly <- merge(x = oly, y = tops, by = "team", all.y = TRUE)
+
+#filter an add empty years
+oly <- oly %>% 
+  filter(total_total>20) %>% 
+  drop_na() %>% 
+  add_row(year = 1916, total = 0) %>%
+  add_row(year = 1940, total = 0)
+
+## streamgraph
+
+#plot
+pal <- c("#065874","#1b9dbf", "#043b52", "#4eb8d7","#086a8d", "#044364",
+         "#0a7796", "#044c5c", "#35b6dc", "#065874",
+         "#1b9dbf","#043b52", "#4eb8d7", "#086a8d","#044364",
+         "#0a7796", "#044c5c", "#35b6dc", "#065874")
+
+# Shape: classic
+p1 <- streamgraph(oly, key="team", value="total", date="year",
+                  width="800px", height="300px", interactive = TRUE) %>% 
+  sg_legend(TRUE, "Team: ") %>% 
+  sg_axis_y(0) %>%
+  sg_fill_manual(values=pal) %>% 
+  sg_annotate(label="World War", x=1916, 
+              y=20, color="black", size=30)
+p1
+
+# save the widget
+saveWidget(p1, "widget.html")
+
+
+
+
+
+
+
+
+
