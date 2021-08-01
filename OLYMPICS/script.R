596ca378fbbbebb960bc5a9a5165318808bcd8df
lau-cloud <44518262+lau-cloud@users.noreply.github.com>
2021-08-01 14:08:09 +0200

    Update script.R

--- OLYMPICS/script.R
+++ OLYMPICS/script.R
@@ -40,29 +40,33 @@ oly <- oly %>%
   add_row(year = 1916, total = 0) %>%
   add_row(year = 1940, total = 0)
 
+class(oly$year)
+oly <- transform(oly, year = as.Date(as.character(year), "%Y"))
+
 ## streamgraph
 
 #plot
 pal <- c("#065874","#1b9dbf", "#043b52", "#4eb8d7","#086a8d", "#044364",
          "#0a7796", "#044c5c", "#35b6dc", "#065874",
          "#1b9dbf","#043b52", "#4eb8d7", "#086a8d","#044364",
-         "#0a7796", "#044c5c", "#35b6dc", "#065874")
+         "#0a7796", "#044c5c", "#F97141", "#065874")
 
 # Shape: classic
 p1 <- streamgraph(oly, key="team", value="total", date="year",
-                  width="800px", height="300px", interactive = TRUE) %>% 
+                  width="1000px", height="300px", interactive = TRUE) %>%
+  sg_axis_x(tick_interval=5, tick_units = "year", tick_format="%Y") %>%
   sg_legend(TRUE, "Team: ") %>% 
   sg_axis_y(0) %>%
   sg_fill_manual(values=pal) %>% 
-  sg_annotate(label="World War", x=1916, 
-              y=20, color="black", size=30)
+  sg_annotate(label="I World War", x=as.Date("1913-08-01"), 
+              y=50, color="black", size=10) %>% 
+  sg_annotate(label="II World War", x=as.Date("1938-08-01"), 
+              y=50, color="black", size=10)
+
 p1
 
 # save the widget
 saveWidget(p1, "widget.html")
-
-
-
 
 
 
